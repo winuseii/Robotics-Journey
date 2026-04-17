e4924a92e40b606178c5896dc8dd44142ca23278
winuseii <winuseii@gmail.com>
2026-04-17 22:43:34 +0530

    Update day1_vectors.py

--- Phase 0/day1_vectors.py
+++ Phase 0/day1_vectors.py
@@ -1,21 +1,27 @@
 # Day 1 - Vector operations without NumPy
-# Goal: understand what these operations mean geometrically
 
 def add_vectors(v1, v2):
     return [a + b for a, b in zip(v1, v2)]
-    pass
 
 def scale_vector(v, scalar):
     return [a*scalar for a in v]   
-    pass
 
 def dot_product(v1, v2):
     return sum(a * b for a, b in zip(v1, v2))
-    pass
 
-# Test it
+# Test vectors:
 v1 = [1, 2, 3]
 v2 = [4, 5, 6]
-print("Sum:", add_vectors(v1, v2))       # expect [5, 7, 9]
-print("Scaled:", scale_vector(v1, 2))    # expect [2, 4, 6]
-print("Dot:", dot_product(v1, v2))       # expect 32
+print("Sum:", add_vectors(v1, v2))       
+print("Scaled:", scale_vector(v1, 2))    
+print("Dot:", dot_product(v1, v2))       
+
+
+# result:
+# Sum: [5, 7, 9]
+# Scaled: [2, 4, 6]
+# Dot: 32
+
+#i learnt about the zip, def, pass and return keywords.
+#i also learnt the difference between return and print keywords.
+#i learnt how to recall a defined function and how to use it with different arguments.
