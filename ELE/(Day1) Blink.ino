a783c0e93191b8e612a59ea264db33fe1c756c90
winuseii <winuseii@gmail.com>
2026-07-14 20:09:01 +0530

    Update and rename Blink.ino to ELE/(Day1) Blink.ino

---
+++ ELE/(Day1) Blink.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+}
+
+// refined my understanding of basic electrical terms (voltage, current and resistance); beyond just the theory my professors tell us to mug up.
+// understood arduino keywords like pinMode() and digitalWrite(). 
+// Made an LED blink usinging an Arduino UNO
